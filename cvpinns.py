327311a91ccc8bb6780ffe25d07d1c5de489dbf7
Ravi Ghanshyam Patel <rgpatel@ceesrvnode1.sandia.gov>
2021-02-21 14:04:11 -0700

    start

--- cvpinns.py
+++ cvpinns.py
@@ -2,8 +2,13 @@ import numpy as np
 import tensorflow as tf
 
 class PDE:
-    def __init__(self,L,nx,quad,F):
-        self.F = F
+    def __init__(self,L,nx,quad,F0,F1,IC,BCl,BCr,u):
+        self.F0 = F0
+        self.F1 = F1
+        self.IC = IC
+        self.BCl = BCl
+        self.BCr = BCr
+        self.u = u
         
         self.L = L
         self.nx = nx
@@ -12,10 +17,10 @@ class PDE:
 
         xi0  = quad['0'] [0]
         xi1  = quad['1'] [0]
-        xi01 = quad['01'][0]
+
         self.wi0  = quad['0'] [1]
         self.wi1  = quad['1'] [1]
-        self.wi01 = quad['01'][1]
+
 
 
         x0 = np.linspace(L[0][0],L[0][1],nx[0]+1)
@@ -23,56 +28,76 @@ class PDE:
 
         x0c = (x0[0:-1]+x0[1:])/2
         x1c = (x1[0:-1]+x1[1:])/2
-        x01c = np.transpose(np.meshgrid(x0c,x1c,indexing='ij'),(1,2,0))
+        xc = np.transpose(np.meshgrid(x0c,x1c,indexing='ij'),(1,2,0))
 
 
-        dx = x01c[1,1] - x01c[0,0]
+        dx = xc[1,1] - xc[0,0]
         self.dx = dx
         
         self.x0  = tf.constant(np.expand_dims(np.transpose(np.meshgrid(x0,x1c,indexing='ij'),(1,2,0)),2) \
                      + np.stack([np.zeros(len(xi0)),dx[1]/2*xi0],1))
         self.x1  = tf.constant(np.expand_dims(np.transpose(np.meshgrid(x0c,x1,indexing='ij'),(1,2,0)),1) \
                      + np.expand_dims(np.stack([dx[0]/2*xi1,np.zeros(len(xi1))],1),1))
-        self.x01 = tf.constant(np.expand_dims(x01c,[1,3]) + np.expand_dims(xi01*dx/2,1))
         
-        self.x = x01c
-       
+        self.x = xc
         
-        @tf.function        
-        def F0(x0i):
-            return tf.concat([
-                        self.F['0L'](tf.expand_dims(x0i[0],0)),   
-                        self.F['0I'](x0i[1:-1]),
-                        self.F['0H'](tf.expand_dims(x0i[-1],0))
-                    ],0)
+        x0r = tf.reshape(self.x0,(nx[0]+1,nx[1]*len(xi0),2))
 
-        
-            
-        @tf.function
-        def F1(x1i):
-            return tf.concat([
-                        self.F['1L'](tf.expand_dims(x1i[:,:,0],2)),
-                        self.F['1I'](x1i[:,:,1:-1]),
-                        self.F['1H'](tf.expand_dims(x1i[:,:,-1],2))
-                    ],2)
+        jrmax = nx[1]*len(xi0)
 
-        
-        @tf.function
-        def F01(x01i):
-            return self.F['01'](x01i)
-        
-        self.F0 = F0
-        self.F1 = F1
-        self.F01 = F01
+        if xi1[0]<-1.+1e-10:
+            jl = 1
+        else:
+            jl = 0
+        if xi1[-1]>1-1e-10:
+            jr = jrmax-1
+        else:
+            jr = jrmax
+
+        self.x0_I = x0r[1::,jl:jr]
+        self.x0_1L = x0r[:,0:jl]
+        self.x0_1R = x0r[:,jr:jrmax]
+        self.x0_0L = tf.expand_dims(x0r[0,jl:jr],0)
+
+
+        x1r = tf.reshape(self.x1,(nx[0]*len(xi1),nx[1]+1,2))
+
+        jrmax = nx[0]*len(xi1)
+
+        if xi0[0]<-1.+1e-10:
+            jl = 1
+        else:
+            jl = 0
+        if xi0[-1]>1-1e-10:
+            jr = jrmax-1
+        else:
+            jr = jrmax
+
+
+        self.xl_I = x1r[1::,1:-1]
+        self.x1_1L = tf.expand_dims(x1r[:,0],1)
+        self.x1_1R = tf.expand_dims(x1r[:,-1],1)
+
+        self.xl_0L = tf.expand_dims(x1r[0,1:-1],0)
 
 
     
     @tf.function
     def getRES(self):
-        F0int  = self.dx[1]/2*tf.tensordot(self.F0(self.x0),self.wi0,[2,0])
-        F1int  = self.dx[0]/2*tf.tensordot(self.F1(self.x1),self.wi1,[1,0])
-        F01int = self.dx[0]*self.dx[1]/4*tf.tensordot(self.F01(self.x01),self.wi01,[[1,3],[0,1]])
+        F0h = tf.concat([self.F0(self.BCl(self.x0_1L)),
+                                    tf.concat([self.F0(self.IC(self.x0_0L)),
+                                           self.F0(self.u(self.x0_I))],0),
+                              self.F0(self.BCr(self.x0_1R))],1)
+        F0i = tf.reshape(F0h,(self.nx[0]+1,self.nx[1],len(self.quad['0'][0]),F0h.shape[-1]))
+        
+        F1h = tf.concat([self.F1(self.BCl(self.x1_1L)),
+                                    tf.concat([self.F1(self.IC(self.xl_0L)),
+                                           self.F1(self.u(self.xl_I))],0),
+                              self.F1(self.BCr(self.x1_1R))],1)
+        F1i = tf.reshape(F1h,(self.nx[0],len(self.quad['1'][0]),self.nx[1]+1,F0h.shape[-1]))
+        
+        F0int  = self.dx[1]/2*tf.tensordot(F0i,self.wi0,[2,0])
+        F1int  = self.dx[0]/2*tf.tensordot(F1i,self.wi1,[1,0])
         return  (F0int[1::] - F0int[0:-1])  \
-               +(F1int[:,1::] - F1int[:,0:-1])  \
-               -F01int
+               +(F1int[:,1::] - F1int[:,0:-1])
  
